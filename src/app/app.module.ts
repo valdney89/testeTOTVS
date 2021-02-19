658c45123a3ffbcac79a793ecac596f7bad9abba
Valdney Nogueira <valdneysouza@gmail.com>
2021-02-19 02:57:18 -0300

    criação dos primeiros components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ViewComponent } from './modules/list/view/view.component';
+import { ListComponent } from './modules/list/list.component';
+import { BewListComponent } from './modules/bew-list/bew-list.component';
+import { NewListComponent } from './modules/new-list/new-list.component';
+import { TasksComponent } from './modules/list/components/tasks/tasks.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { MenuComponent } from './core/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent,
+    ListComponent,
+    BewListComponent,
+    NewListComponent,
+    TasksComponent,
+    FooterComponent,
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
